56a3c7bb07fd87bb658b5edc1b009e2724352668
Dysiah Bell <dysiaht.bell@gmail.com>
2024-02-15 18:19:30 -0500

    Add project files.

---
+++ ChessLogic/Player.cs
@@ -0,0 +1,23 @@
+﻿
+namespace ChessLogic
+{
+    public enum Player // Will be used to store which player's turn it is and who won the game, as well as, represent the color of the chess pieces
+    {
+        None,
+        White,
+        Black
+    }
+
+    public static class PlayerExtensions
+    {
+        public static Player Opponent(this Player player) // Will take a player as parameter and return that player's opponent
+        {
+            return player switch
+            {
+                Player.White => Player.Black,
+                Player.Black => Player.White,
+                _ => Player.None,
+            };
+        }
+    }
+}
